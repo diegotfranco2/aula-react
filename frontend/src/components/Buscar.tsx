9a5bd5d5d5ccbc15007246d08dba12534b58e20b
Diego Franco <diegotavaresfranco123@gmail.com>
2024-10-17 19:59:50 -0400

    metade aula 17/10

--- frontend/src/components/Buscar.tsx
+++ frontend/src/components/Buscar.tsx
@@ -2,13 +2,13 @@ import { MdOutlineSearch } from "react-icons/md";
 
 const Buscar = () => {
   return (
-    <div className="border border-gray-300 flex items-center rounded px-2 bg-gray-50">
+    <div className="border border-gray-300 flex items-center rounded px-2 py-0.5 bg-gray-50">
       <input
         type="text"
-        className="h-8 border-none outline-0 rounded bg-inherit"
+        className="border-none outline-0 rounded bg-inherit py-0.5"
         placeholder="Buscar..."
       />
-      <button className="flex justify-center items-center h-4 bg-inherit border-none text-gray-500 cursor-pointer">
+      <button className="flex justify-center items-center p-1 text-gray-500 outline-0 rounded focus:ring-2">
         <MdOutlineSearch />
       </button>
     </div>);
